4c3f1f1f4dbe2661d05a49d3f5f9e7b1f5532800
VishalChoudhary <vishalghn953@gmail.com>
2025-11-04 22:04:38 +0530

    Home Page with useState finish commit

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,45 +1,61 @@
-import React, { useState } from 'react';
-import './styles.css';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import CardsData from './CardData';
+import React, { useState } from "react";
+import "./styles.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import CardsData from "./CardData";
 
 const Home = () => {
-  
   const [cardData, useCardData] = useState(CardsData);
-  
+
   return (
     <>
-      <section className='iteam_section mt-4 container'>
-        <h2 className='px-4' style={{fontWeight:400}}>Restaurants in Guwahati Open Now!</h2>
-        <div className='row mt-2 d-flex justify-content-around align-items-center'>
-          <Card style={{width:'22rem',border:'none'}} className='hove mb-4'>
-            <Card.Img src='/vite.svg' />
-            <div className='card_body'>
-              <div className='upper_data d-flex justify-content-between align-items-center'>
-                <h4 className='mt-2'>Punjabi</h4>
-                <span>3.8&nbsp;★</span>
-              </div>
+      <section className="iteam_section mt-4 container">
+        <h2 className="px-4" style={{ fontWeight: 400 }}>
+          Restaurants in Guwahati Open Now!
+        </h2>
+        <div className="row mt-2 d-flex justify-content-around align-items-center">
+          {cardData.map((element, index) => {
+            return (
+              <Card
+                style={{ width: "22rem", border: "none" }}
+                className="hove mb-4"
+              >
+                <Card.Img variant="top" className="cd" src={element.imgdata} />
+                <div className="card_body">
+                  <div className="upper_data d-flex justify-content-between align-items-center">
+                    <h4 className="mt-2">{element.dish}</h4>
+                    <span>{element.rating}&nbsp;★</span>
+                  </div>
 
-              <div className='lower_data d-flex justify-content-between'>
-                <h5>North Indian, Biryani, Mughlai</h5>
-                <span>300</span>
-              </div>
-              <div className="extra"></div>
+                  <div className="lower_data d-flex justify-content-between">
+                    <h5>{element.address}</h5>
+                    <span>{element.price}</span>
+                  </div>
+                  <div className="extra"></div>
 
-              <div className='last_data d-flex justify-content-between align-items-center'>
-                <img src='/vite.svg' className='limg' alt=''/>
-                <Button style={{width:'150px', background:'#ff3054db', border:'none'}} variant='outline-light'
-                  className='mt-2 mb-2'
-                >Add To Cart</Button>
-                <img src='/vite.svg' className='laimg' alt=''/>
-              </div>
-            </div>
-          </Card>
+                  <div className="last_data d-flex justify-content-between align-items-center">
+                    <img src={element.arrimg} className="limg" alt="" />
+                    <Button
+                      style={{
+                        width: "150px",
+                        background: "#ff3054db",
+                        border: "none",
+                      }}
+                      variant="outline-light"
+                      className="mt-2 mb-2"
+                    >
+                      Add To Cart
+                    </Button>
+                    <img src={element.delimg} className="laimg" alt="" />
+                  </div>
+                </div>
+              </Card>
+            );
+          })}
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
