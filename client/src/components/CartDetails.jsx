e5b1ec577400296228237eed39cd503e6be1fabf
VishalChoudhary <vishalghn953@gmail.com>
2025-11-05 21:07:28 +0530

    qnty increment commit

--- client/src/components/CartDetails.jsx
+++ client/src/components/CartDetails.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./cartStyles.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../redux/cartSlice";
 
 const CartDetails = () => {
   
   const {carts} = useSelector((state)=>state.allCart);
+  const dispatch = useDispatch();
+  //add to cart
+  const handleIncrement = (e) =>{
+    dispatch(addToCart(e));
+  }
 
   return (
     <>
@@ -13,7 +19,7 @@ const CartDetails = () => {
           <div className="card">
             <div className="card-header bg-dark p-3">
               <div className="card-header-flex">
-                <h5 className="text-white m-0">Cart Calculation(1)</h5>
+                <h5 className="text-white m-0">Cart Calculation{carts.length>0 ? `(${carts.length})`: ""}</h5>
                 {carts.length > 0 ? (
                   <button className="btn btn-danger mt-0 btn-sm">
                     <i className="fa fa-trash-alt mr-2"></i>
@@ -64,20 +70,20 @@ const CartDetails = () => {
                             </td>
                             <td><div className="product-img"><img src={data.imgdata} /></div></td>
                             <td><div className="product-img"><p>{data.dish}</p></div></td>
-                            <td>300</td>
+                            <td>{data.price}</td>
                             <td>
                               <div className="prdct-qty-container">
                                 <button className="prdct-qty-btn" type="button">
                                   <i className="fa fa-minus"></i>
                                 </button>
                                 <input type="text" name="" id=""  className="qty-input-box" value={data.qnty} disabled/>
-                                <button className="prdct-qty-btn" type="button">
+                                <button className="prdct-qty-btn" type="button" onClick={()=>handleIncrement(data)}>
                                   <i className="fa fa-plus"></i>
                                 </button>
                               </div>
                             </td>
                             <td className="text-right">
-                              {data.price}
+                              {data.qnty*data.price}
                             </td>
                           </tr>
                         </>
